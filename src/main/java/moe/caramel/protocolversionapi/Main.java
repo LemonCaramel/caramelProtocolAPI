deb58fd7d6b240ad1caad1ef680aa7a34189afc2
LemonCaramel <admin@caramel.moe>
2021-05-25 01:01:15 +0900

    new method

--- src/main/java/moe/caramel/protocolversionapi/Main.java
+++ src/main/java/moe/caramel/protocolversionapi/Main.java
@@ -11,19 +11,23 @@ public final class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         api = new API(this);
-        getServer().getScheduler().runTask(this, () -> { // Very Bad Design
-            try {
-                serverVersion = (int) getObject(getServer(),
-                        "getServer" /* DedicatedServer */,
-                        "getServerPing" /* ServerPing */,
-                        "getServerData" /* ServerData */,
-                        "getProtocolVersion") /* Version */;
-            } catch (NoSuchMethodException | IllegalAccessException | NumberFormatException |
-                    ClassCastException | java.lang.reflect.InvocationTargetException exception) {
-                exception.printStackTrace();
-                getServer().shutdown();
-            }
-        });
+        try {
+            serverVersion = this.getServer().getUnsafe().getProtocolVersion();
+        } catch (Exception ignored) {
+            getServer().getScheduler().runTask(this, () -> { // Very Bad Design
+                try {
+                    serverVersion = (int) getObject(getServer(),
+                            "getServer" /* DedicatedServer */,
+                            "getServerPing" /* ServerPing */,
+                            "getServerData" /* ServerData */,
+                            "getProtocolVersion") /* Version */;
+                } catch (NoSuchMethodException | IllegalAccessException | NumberFormatException |
+                        ClassCastException | java.lang.reflect.InvocationTargetException exception) {
+                    exception.printStackTrace();
+                    getServer().shutdown();
+                }
+            });
+        }
     }
 
     private Object getObject(Object object, String... methods)
