fb3316ba3f78ebec6373313ae86cebb10a135241
LemonCaramel <admin@caramel.moe>
2022-05-14 19:41:56 +0900

    [2.0.3] protocol version update

--- src/main/java/moe/caramel/protocolapi/Protocol.java
+++ src/main/java/moe/caramel/protocolapi/Protocol.java
@@ -5,19 +5,16 @@ import java.util.TreeSet;
 
 public enum Protocol {
 
-    MINECRAFT_1_15_2(578, "1.15.2"),
-    MINECRAFT_1_16(735, "1.16"),
-    MINECRAFT_1_16_1(736, "1.16.1"),
-    MINECRAFT_1_16_2(751, "1.16.2"),
-    MINECRAFT_1_16_3(753, "1.16.3"),
     MINECRAFT_1_16_4(754, "1.16.4/5"),
     MINECRAFT_1_17(755, "1.17"),
     MINECRAFT_1_17_1(756, "1.17.1"),
+    MINECRAFT_1_18(757, "1.18/1.18.1"),
+    MINECRAFT_1_18_2(758, "1.18.2"),
     MINECRAFT_UNKNOWN(-1, "UNKNOWN");
 
     public final int number;
     public final String version;
-    public static final Protocol STABLE = MINECRAFT_1_17_1;
+    public static final Protocol STABLE = MINECRAFT_1_18_2;
 
     Protocol(int number, String version) {
         this.number = number;
